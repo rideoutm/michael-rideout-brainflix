e4f57cbbdb0a46017bb0fa3bc87cd9b64e5e8712
Michael Rideout <rideout.m@hotmail.com>
2022-09-04 16:29:43 -0400

    resolve issue in upload.js

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,3 +1,110 @@
+// import "./upload.scss";
+// import { Link, useNavigate } from "react-router-dom";
+// import HomePage from "../HomePage/HomePage";
+// import bikeImage from "../../Assets/Images/Upload-video-preview.jpg";
+// import { useRef } from "react";
+// import axios from "axios";
+
+// function Upload() {
+//   const input = useRef();
+//   const textInput = useRef();
+//   let navigate = useNavigate();
+
+//   const changeBorderColTitle = (e) => {
+//     console.log(e.target);
+//     if (e.target.value.length > 0) input.current.style.borderColor = "#e1e1e1";
+//   };
+
+//   const changeBorderColText = (e) => {
+//     if (e.target.value.length > 0)
+//       textInput.current.style.borderColor = "#e1e1e1";
+//   };
+
+//   // Send post request on button click, validate forms
+//   const btnClick = (e) => {
+//     e.preventDefault();
+//     const title = e.target.titlebox.value;
+//     const textarea = e.target.textarea.value;
+
+//     let newVideo = {
+//       title: title,
+//       description: textarea,
+//     };
+
+//     // Add error border color if fields are empty.
+//     if (title === "") input.current.style.borderColor = "#D22D2D";
+//     if (textarea === "") textInput.current.style.borderColor = "#D22D2D";
+//     if (title === "" || textarea === "") return;
+
+//     axios.post("http://localhost:8080/", newVideo).then((res) => {
+//       if (res.status >= 200 && res.status < 300) {
+//         navigate("/UploadComp", { replace: true });
+//       }
+//     });
+//   };
+
+//   return (
+//     <form onSubmit={btnClick} className="Upload__container">
+//       <hr className="Upload__divider" />
+//       <div className="Upload__header">Upload Video</div>
+//       <hr className="Upload__divider-top" />
+//       <div className="Upload__desktop-Container">
+//         <div className="Upload__img-container">
+//           <label className="Upload__thumbnail-label" htmlFor="video-thumbnail">
+//             VIDEO THUMBNAIL
+//           </label>
+
+//           <img className="Upload__img" src={bikeImage} alt="upload thumbnail" />
+//         </div>
+//         <div className="Upload__inputs">
+//           <label className="Upload__title-label" htmlFor="title-label">
+//             TITLE YOUR VIDEO
+//           </label>
+//           <input
+//             id="title-label"
+//             onChange={changeBorderColTitle}
+//             ref={input}
+//             className="Upload__video-title"
+//             type="text"
+//             placeholder="Add a title to your video"
+//             name="titlebox"
+//           />
+//           <label
+//             className="Upload__description-label"
+//             htmlFor="video-description"
+//           >
+//             ADD A VIDEO DESCRIPTION
+//           </label>
+//           <textarea
+//             ref={textInput}
+//             onChange={changeBorderColText}
+//             id="video-description"
+//             className="Upload__video-description"
+//             type="text"
+//             placeholder="Add a description to your video"
+//             name="textarea"
+//           />
+//         </div>
+//       </div>
+//       <hr className="Upload__divider-bottom" />
+//       <div className="Upload__btn-container">
+//         <div className="Upload__publish-container">
+//           <button onClick={btnClick} className="Upload__publish">
+//             PUBLISH
+//           </button>
+//         </div>
+
+//         <div className="Upload__cancel-container">
+//           <Link to="/" element={HomePage}>
+//             <button className="Upload__cancel">CANCEL</button>
+//           </Link>
+//         </div>
+//       </div>
+//     </form>
+//   );
+// }
+
+// export default Upload;
 import "./upload.scss";
 import { Link, useNavigate } from "react-router-dom";
 import HomePage from "../HomePage/HomePage";
@@ -12,6 +119,7 @@ function Upload() {
 
   const changeBorderColTitle = (e) => {
     console.log(e.target);
+
     if (e.target.value.length > 0) input.current.style.borderColor = "#e1e1e1";
   };
 
@@ -20,7 +128,6 @@ function Upload() {
       textInput.current.style.borderColor = "#e1e1e1";
   };
 
-  // Send post request on button click, validate forms
   const btnClick = (e) => {
     e.preventDefault();
     const title = e.target.titlebox.value;
@@ -31,7 +138,6 @@ function Upload() {
       description: textarea,
     };
 
-    // Add error border color if fields are empty.
     if (title === "") input.current.style.borderColor = "#D22D2D";
     if (textarea === "") textInput.current.style.borderColor = "#D22D2D";
     if (title === "" || textarea === "") return;
@@ -89,9 +195,9 @@ function Upload() {
       <hr className="Upload__divider-bottom" />
       <div className="Upload__btn-container">
         <div className="Upload__publish-container">
-          <button onClick={btnClick} className="Upload__publish">
-            PUBLISH
-          </button>
+          {/* <Link to="/UploadComp" element={UploadComplete}> */}
+          <button className="Upload__publish">PUBLISH</button>
+          {/* </Link> */}
         </div>
 
         <div className="Upload__cancel-container">
